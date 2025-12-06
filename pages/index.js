ec9c2ab14f11cce57efc6e326741cab33a67547f
ryasumaro <ryasumaro@gmail.com>
2025-12-06 13:59:25 +0000

    adiciona scripts lint:check e lint:fix

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Aqui estou iniciando meus estudos de programação</h1>
+function Home() {
+  return <h1>Aqui estou iniciando meus estudos de programação</h1>;
 }
 
 export default Home;
